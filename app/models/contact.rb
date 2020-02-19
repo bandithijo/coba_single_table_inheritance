8a0fa37cf3a54dcbab738ffbc9cac69fd09fb596
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-02-19 10:56:19 +0800

    Add scope friends and emergencies for contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ApplicationRecord
+  scope :friends,   -> { where(type: 'Friend') }    # Contact.friends
+  scope :emergency, -> { where(type: 'Emergency') } # Contact.emergencies
 end
